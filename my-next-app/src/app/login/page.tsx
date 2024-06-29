2713520cb4a2827083457a042387af18614d7d81
kizunai9 <yaodong.sun@outlook.com>
2024-06-29 23:24:58 +0200

    dd

---
+++ my-next-app/src/app/login/page.tsx
@@ -0,0 +1,15 @@
+// pages/index.tsx
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Homjdse() {
+  return (
+    <main className='flex min-h-screen flex-col justify-between p-10'>
+
+      <div>
+        <p>helo</p>
+        <p>helo</p>
+      </div>
+    </main>
+  );
+}
